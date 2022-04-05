9dfb06bf3b15daaae8e0e742d0a58f14ee0b72b7
Elton Henrique <eltonhst34@gmail.com>
2022-04-05 10:00:01 -0300

    teste

--- 0x01/spring_boot_1/com/example/demo/MessageResource.java
+++ 0x01/spring_boot_1/com/example/demo/MessageResource.java
@@ -1,4 +1,3 @@
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +9,19 @@ public class MessageResource {
     @GetMapping("/simpleMessageWelcome")
     public String simpleMessageWelcome() {
         return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
+
+    }
+
+    @GetMapping("/login")
+    public String login(String user, String password) {
+        if (user == null || password == null) {
+            return "USUÁRIO E SENHA NÃO INFORMADOS";
+        } else if (user.length() > 15 || password.length() > 15) {
+            return "USUÁRIO E SENHA INVÁLIDOS";
+        } else {
+            return "LOGIN EFETUADO COM SUCESSO !!!";
+        }
+
     }
 
 }
